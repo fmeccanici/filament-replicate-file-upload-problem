47c186130c4b30d7b994899f206aab7d9ac8c789
fmeccanici <florismeccanici@mecctech-solutions.nl>
2025-02-15 13:05:23 +0100

    Initialize repository with replication of problem

---
+++ app/Models/Order.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Order extends Model
+{
+    use InteractsWithMedia;
+
+}
